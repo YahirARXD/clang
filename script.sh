18b5bb3ba548458f495b35b98ff1a410f5fa4322
YahirARXD <98426019+YahirARXD@users.noreply.github.com>
2022-03-21 19:07:07 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,3 +3,5 @@ apt install clang -y
 echo "g++ -g *.cpp && echo 'compilado'" >> $HOME/../usr/bin/cc
 chmod 777 $HOME/../usr/bin/cc
 termux-setup-storage
+echo "Creo  que todo esta listo 💗"
+echo "Te amo mucho mi niña 💗"
